bd83e3e3e1c88d887753ccdde09bfa33a11f2bbf
Blaise Fonseca <blaise.fonseca@un.org>
2023-07-10 12:53:06 -0400

    fix path issue

--- pages/nbsap-targets/my-country/[identifier]/view.vue
+++ pages/nbsap-targets/my-country/[identifier]/view.vue
@@ -7,7 +7,7 @@
 
 <script setup lang="ts">
   import { KmSuspense } from "@/components/controls";
-  import viewTarget from '@components/pages/nbsap-targets/my-country/view-target.vue';
+  import viewTarget from '@/components/pages/nbsap-targets/my-country/view-target.vue';
   import { useRoute } from 'vue-router';
 
   const route           = useRoute();
