7128449092e8d4add4fb760f700fcb044d7e39b2
Blaise Fonseca <blaise.fonseca@un.org>
2023-08-04 16:51:36 -0400

    use better function to remove empty

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,27 +1,23 @@
 import _ from 'lodash';
 import $ from 'jquery';
 
-//TODO revisit logic
-export const removeEmpty = function(obj){
-    if(!obj)
-        return;
-    let newObj;
-    Object.entries(obj).forEach(([k, v]) => {
-      if (_.isArray(v) && v.length) {
-        newObj[k] = _.compact(v);
-      }
-      else if (v === Object(v)) {
-        const newV = removeEmpty(v)
-        if(newV && newV != null){
-          newObj = newObj || {};
-          newObj[k] = newV;
+export const removeEmpty = (obj)=> {
+
+    return function remove(current) {
+      _.forOwn(current, function (value, key) {
+        if (_.isUndefined(value) || _.isNull(value) || _.isNaN(value) ||
+          (_.isString(value) && _.isEmpty(value)) ||
+          (_.isObject(value) && _.isEmpty(remove(value)))) {
+  
+          delete current[key];
         }
-      } else if (v && v != null) {
-        newObj = newObj || {};
-        newObj[k] = obj[k];
-      }
-    });
-    return newObj;
+      });
+
+      if (_.isArray(current)) _.pull(current, undefined);
+  
+      return current;
+  
+    }(_.cloneDeep(obj));
 }
 
 
