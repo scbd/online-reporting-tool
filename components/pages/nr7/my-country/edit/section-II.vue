6ab02685c43ced584d8f14e41aa469633472d267
Blaise Fonseca <blaise.fonseca@un.org>
2023-09-21 11:48:28 -0400

    section II

--- components/pages/nr7/my-country/edit/section-II.vue
+++ components/pages/nr7/my-country/edit/section-II.vue
@@ -1,34 +1,92 @@
 <template>
     <CCard>
       <CCardHeader>
-        <slot name="header"> NR7 Section I</slot>
+        <slot name="header">{{t('sectionII')}} {{t('sectionIIDescription')}}</slot>
       </CCardHeader>
       <CCardBody>
 
-        <div  v-if="isLoading">
+        <div  v-if="nationalReportStore.isBusy">
             <km-spinner></km-spinner>
         </div>
-        <form v-if="!isLoading" name="editForm">          
-            <km-form-workflow :focused-tab="props.workflowActiveTab" :get-document="onGetDocument" :validation-report="validationReport" 
-                :container="container" :on-pre-close="onClose" :on-post-save-draft="onPostSaveDraft" hidden-tabs="['introduction', 'publish']">
+        <form v-if="!nationalReportStore.isBusy && nationalReportStore.nationalReportDraft" name="editForm">          
+            <nr7-workflow :focused-tab="props.workflowActiveTab" :get-document="cleanDocument" :validation-report="validationReport" 
+                :container="container" :on-pre-close="onClose" :on-post-save-draft="onPostSaveDraft">
                 <template #submission>
                     <km-form-group>
                         <div class="card">
-                            <div class="card-header bg-secondary">
-                                Elements of the global targets
-                            </div>
-                            <div class="card-body">                      
-                                <km-form-group required caption="Elements of the global targets addressed by national targets" name="elementOfGlobalTargetsInfo">
-                                    <km-input-rich-lstring v-model="document.elementOfGlobalTargetsInfo" :locales="document.header.languages"></km-input-rich-lstring>
-                                </km-form-group>                                    
+                            <div class="card-body">    {{ sectionII }}                  
+                                <km-form-group name="hasRevisedNbsap" :caption="t('revisedNbsap')">                                       
+                                    <km-form-check-group>
+                                        <km-form-check-item type="radio" name="hasRevisedNbsap"  for="hasRevisedNbsap" id="hasRevisedNbsapYes"       value="yes"        v-model="sectionII.hasRevisedNbsap.value" :label="t('yes')"/>
+                                        <km-form-check-item type="radio" name="hasRevisedNbsap"  for="hasRevisedNbsap" id="hasRevisedNbsapNo"        value="no"         v-model="sectionII.hasRevisedNbsap.value" :label="t('no')"/>
+                                        <km-form-check-item type="radio" name="hasRevisedNbsap"  for="hasRevisedNbsap" id="hasRevisedNbsapInProcess" value="inProcess"  v-model="sectionII.hasRevisedNbsap.value" :label="t('inProcess')"/>
+                                    </km-form-check-group>                                    
+                                </km-form-group>                       
+                                <km-form-group name="hasRevisedNbsap.additionalInformation" :caption="t('indicateCompletionDate')"
+                                    v-if="sectionII.hasRevisedNbsap && (sectionII.hasRevisedNbsap.value=='no' || sectionII.hasRevisedNbsap.value == 'inProcess')">  
+                                    <CFormInput type="text" id="additionalInformation" v-model="sectionII.hasRevisedNbsap.additionalInformation"/>                             
+                                </km-form-group>   
+                                
+                                <km-form-group name="hasStakeholderEngagement" :caption="t('stakeholderEngagement')">                                       
+                                    <km-form-check-group>
+                                        <km-form-check-item type="radio" name="hasStakeholderEngagement"  for="hasStakeholderEngagement" id="hasStakeholderEngagementTrue"       :value="true"        v-model="sectionII.hasStakeholderEngagement" :label="t('yes')"/>
+                                        <km-form-check-item type="radio" name="hasStakeholderEngagement"  for="hasStakeholderEngagement" id="hasStakeholderEngagementFalse"      :value="false"       v-model="sectionII.hasStakeholderEngagement" :label="t('no')"/>
+                                    </km-form-check-group>                                    
+                                </km-form-group> 
+                                <km-form-group name="stakeholders" :caption="t('stakeholders')" v-if="sectionII.hasStakeholderEngagement">    
+                                            <!--   -->                                   
+                                    <km-select
+                                            v-model="sectionII.stakeholders"
+                                            class="validationClass"
+                                            label="title"
+                                            track-by="identifier"
+                                            value-key="identifier"
+                                            :placeholder="t('stakeholders')"
+                                            :multiple="true"
+                                            :disabled="false"    
+                                            :options="stakeholderLists"   
+                                            :custom-label="customLabel"
+                                            :custom-selected-item="customSelectedItem"                                    
+                                        >
+                                        </km-select>
+                                </km-form-group>
+
+                                <km-form-group name="hasNbsapAdopted" :caption="t('hasNbsapAdopted')">                                       
+                                    <km-form-check-group>
+                                        <km-form-check-item type="radio" name="hasNbsapAdopted"  for="hasNbsapAdopted" id="hasNbsapAdoptedYes"       value="yes"        v-model="sectionII.hasNbsapAdopted.value" :label="t('yes')"/>
+                                        <km-form-check-item type="radio" name="hasNbsapAdopted"  for="hasNbsapAdopted" id="hasNbsapAdoptedNo"        value="no"         v-model="sectionII.hasNbsapAdopted.value" :label="t('no')"/>
+                                        <km-form-check-item type="radio" name="hasNbsapAdopted"  for="hasNbsapAdopted" id="hasNbsapAdoptedInProcess" value="inProcess"  v-model="sectionII.hasNbsapAdopted.value" :label="t('inProcess')"/>
+                                    </km-form-check-group>                                    
+                                </km-form-group>                       
+                                <km-form-group name="hasNbsapAdopted.additionalInformation" :caption="t('indicateAdoptionDate')"
+                                    v-if="sectionII.hasNbsapAdopted && (sectionII.hasNbsapAdopted.value=='no' || sectionII.hasNbsapAdopted.value == 'inProcess')">                                       
+                                    <CFormInput type="text" id="additionalInformation" v-model="sectionII.hasNbsapAdopted.additionalInformation"/>                             
+                                </km-form-group>  
+
+                                <km-form-group name="policyInstrument" :caption="t('policyInstrument')" v-if="(sectionII.hasNbsapAdopted||{}).value=='yes'">   
+                                    <km-select
+                                            v-model="sectionII.policyInstrument"
+                                            class="validationClass"
+                                            label="title"
+                                            track-by="identifier"
+                                            value-key="identifier"
+                                            :placeholder="t('stakeholders')"
+                                            :multiple="false"
+                                            :disabled="false"    
+                                            :options="policyInstrumentLists"   
+                                            :custom-label="customLabel"
+                                            :custom-selected-item="customSelectedItem"                                    
+                                        >
+                                        </km-select>
+                                </km-form-group>
                             </div>
                         </div>
                     </km-form-group>
                 </template>
                 <template #review>                
-                    <view-nr7-section-I :identifier="document.header.identifier" :document="cleanDocument"></view-nr7-section-I>
+                    <view-nr7-section-II :identifier="nationalReportStore.nationalReportDraft.header.identifier" :document="nationalReportStore.nationalReportDraft"></view-nr7-section-II>
                 </template>
-            </km-form-workflow>
+            </nr7-workflow>
             <km-modal-spinner :visible="showSpinnerModal" v-if="showSpinnerModal"></km-modal-spinner>
         </form>
 
@@ -36,23 +94,24 @@
     </CCard>
   
 </template>
-
+<i18n src="@/i18n/dist/components/pages/nr7/my-country/edit/section-II.json"></i18n>
 <script setup>
   
     import { useAsyncState } from '@vueuse/core'
     import { KmInputRichLstring, KmSelect, KmFormGroup, KmValidationErrors,KmGovernment, KmLanguages,
-        KmFormCheckGroup, KmFormCheckItem, KmInputLstring,KmSpinner, KmFormWorkflow
+        KmFormCheckGroup, KmFormCheckItem, KmInputLstring,KmSpinner
     } from "~/components/controls";
-    // import viewTarget               from "./view-target-part-2.vue";
+    import Nr7Workflow              from './NR7Workflow.vue'
+    import viewNr7SectionII         from "@/components/pages/nr7/my-country/view/section-II.vue";
     import { useRealmConfStore }    from '@/stores/realmConf';
-    import { useKmDocumentDraftsStore }    from '@/stores/kmDocumentDrafts';
+    import { useNationalReport7Store }    from '@/stores/nationalReport7';
     import { useRoute } from 'vue-router' 
     import { useToast } from 'vue-toast-notification';
     import { useStorage } from '@vueuse/core'
     import { EditFormUtility } from "@/services/edit-form-utility";
 
     const props = defineProps({
-        workflowActiveTab  : {type:Number, default:1 },
+        workflowActiveTab  : {type:Number, default:0 },
         onClose            : {type:Function, required:false},
         onPostSaveDraft    : {type:Function, required:false},
     }) 
@@ -61,18 +120,63 @@
     const security        = useSecurity();
     const route           = useRoute();
     const {$appRoutes:appRoutes }   = useNuxtApp();
-    const locale          = useI18n().locale
-    const $toast                = useToast();        
+    const {t, locale }              = useI18n();
+    const $toast                    = useToast();        
     const container = useAttrs().container;
+    const nationalReportStore = useNationalReport7Store();
 
-    let document = {
-        header : {
-            schema : SCHEMAS.NATIONAL_REPORT_7,
-            identifier : useGenerateUUID(),
-            languages  : EditFormUtility.getPreferredEditLanguages()
-        },        
-        government : {
-            identifier : user.value?.government
-        },
+    await nationalReportStore.loadNationalReportDraft()
+
+    let sectionII = nationalReportStore.nationalReportDraft.sectionII;
+    if(!sectionII){
+        nationalReportStore.nationalReportDraft.sectionII = sectionII = toRef({});
+    }
+    if(!sectionII.hasRevisedNbsap){
+        sectionII.hasRevisedNbsap = {};
+    }
+    if(!sectionII.hasNbsapAdopted){
+        sectionII.hasNbsapAdopted = {};
+    }
+    if(!sectionII.hasStakeholderEngagement){
+        sectionII.hasStakeholderEngagement = {};
+    }
+
+
+    const stakeholderLists = [
+        {identifier: '1', title: 'Indigenous peoples and local communities,,'},
+        {identifier: '1', title: 'Focal points for other multilateral environmental agreements,'},
+        {identifier: '1', title: 'The Protocols and SDGs,  '},
+        {identifier: '1', title: 'Subnational governments,'},
+        {identifier: '1', title: 'Cities and other local authorities,'},
+        {identifier: '1', title: 'Non-governmental organizations,'},
+        {identifier: '1', title: 'Women\'s groups, youth groups,'},
+        {identifier: '1', title: 'The business and finance community,'},
+        {identifier: '1', title: 'The scientific community,'},
+        {identifier: '1', title: 'Academia,'},
+        {identifier: '1', title: 'Faith-based organizations'},
+        {identifier: '1', title: 'Representatives of sectors related to or dependent on biodiversity,'},
+        {identifier: '1', title: 'Citizens at large,'},
+        {identifier: '1', title: 'Other stakeholders'},
+        
+    ]
+    const policyInstrumentLists = [
+        {identifier: '1', title: 'Adopted through legislation or otherwise by parliament'},
+        {identifier: '1', title: 'Adopted by Council of Ministers, Office of the President/Prime Minister or equivalent whole-of-government body'},
+        {identifier: '1', title: 'Adopted by Ministry of Environment or other sectoral ministry'},
+        {identifier: '1', title: 'Integrated into the poverty reduction strategy, sustainable development strategy, national development plan, and other related strategies or plans'},
+        {identifier: '1', title: 'Other (please specify)'},
+    ]
+
+    const customLabel = ({title})=>{        
+        return lstring(title, locale.value);
+    }
+    const customSelectedItem = (item)=>{
+        return { identifier : item };
+    }
+
+
+    function cleanDocument(){
+        const  clean = useKmStorage().cleanDocument({...nationalReportStore.nationalReportDraft});
+        return toRef(clean);
     }
 </script>
---
+++ components/pages/nr7/my-country/view/section-II.vue
@@ -0,0 +1,106 @@
+<template>
+    <CCard>
+      <CCardHeader v-if="identifier">
+        <slot name="header"> {{t('sectionI')}} </slot>
+      </CCardHeader>
+      <CCardBody>
+        <div v-if="document.sectionII">
+        
+            <km-form-group  v-if="document.sectionII.hasRevisedNbsap">
+                <div class="card">
+                    <div class="card-body">  
+                        <km-form-group v-if="document.sectionII.hasRevisedNbsap.value">
+                            <label class="form-label" for="revisedNbsap">{{t('revisedNbsap')}}</label>                           
+                            <km-value>{{ t(`${document.sectionII.hasRevisedNbsap.value}`) }}</km-value>        
+                        </km-form-group>
+                        <km-form-group v-if="document.sectionII.hasRevisedNbsap.additionalInformation">
+                            <label class="form-label" for="indicateCompletionDate">{{t('indicateCompletionDate')}}</label>                           
+                            <km-value>{{document.sectionII.hasRevisedNbsap.additionalInformation}}</km-value>        
+                        </km-form-group>
+                    </div>
+                </div>
+            </km-form-group>
+            <km-form-group  v-if="document.sectionII.hasStakeholderEngagement!=undefined">
+                <div class="card">
+                    <div class="card-body">  
+                        <km-form-group>
+                            <label class="form-label" for="stakeholderEngagement">{{t('stakeholderEngagement')}}</label>                           
+                            <km-value>{{ document.sectionII.hasStakeholderEngagement ? t('yes') : t('no') }}</km-value>        
+                        </km-form-group>
+                        <km-form-group v-if="document.sectionII.stakeholders">
+                            <label class="form-label" for="stakeholders">{{t('stakeholders')}}</label>             
+                            <km-value-terms :value="document.sectionII.stakeholders" :locale="selectedLocale"></km-value-terms>     
+                        </km-form-group>
+                    </div>
+                </div>
+            </km-form-group>
+
+            <km-form-group  v-if="document.sectionII.hasNbsapAdopted">
+                <div class="card">
+                    <div class="card-body">  
+                        <km-form-group v-if="document.sectionII.hasNbsapAdopted.value">
+                            <label class="form-label" for="hasNbsapAdopted">{{t('hasNbsapAdopted')}}</label>                           
+                            <km-value>{{ t(`${document.sectionII.hasNbsapAdopted.value}`) }}</km-value>        
+                        </km-form-group>
+                        <km-form-group v-if="document.sectionII.hasNbsapAdopted.additionalInformation">
+                            <label class="form-label" for="indicateAdoptionDate">{{t('indicateAdoptionDate')}}</label>                           
+                            <km-value>{{document.sectionII.hasNbsapAdopted.additionalInformation}}</km-value>        
+                        </km-form-group>
+                    </div>
+                </div>
+            </km-form-group>
+
+            <km-form-group  v-if="document.sectionII.policyInstrument">
+                <div class="card">
+                    <div class="card-body">  
+                        <km-form-group v-if="document.sectionII.policyInstrument">
+                            <label class="form-label" for="policyInstrument">{{t('policyInstrument')}}</label>             
+                            <km-value-terms :value="document.sectionII.policyInstrument" :locale="selectedLocale"></km-value-terms>     
+                        </km-form-group> 
+                    </div>
+                </div>
+            </km-form-group>
+        </div>
+       
+      </CCardBody>
+    </CCard>
+  
+</template>
+
+<i18n src="@/i18n/dist/components/pages/nr7/my-country/edit/section-II.json"></i18n>
+<i18n src="@/i18n/dist/components/pages/nr7/my-country/view/section-II.json"></i18n>
+
+<script setup>
+  
+    import { KmFormGroup, KmSpinner, KmLstringValue,
+        KmLocales, KmValueTerm, KmValueBool, KmValueTerms, KmValue
+    } from "~/components/controls";
+    import { mapStores }            from 'pinia'
+    import { languages }            from '@/app-data/languages'
+    import { degreeOfAlignments }   from '@/app-data/degreeOfAlignments';
+    import { useThesaurusStore }    from '@/stores/thesaurus';
+    import { useCountriesStore }    from '@/stores/countries';
+    import { useRealmConfStore }    from '@/stores/realmConf';
+    import { useRoute } from 'vue-router' 
+    import { KmDocumentDraftsService}from "@/services/kmDocumentDrafts";
+    import { KmDocumentsService } from "@/services/kmDocuments";
+    
+
+    const { user }                = useAuth();
+    const security                = useSecurity();
+    const route                   = useRoute();
+    const {t, locale}             = useI18n();
+    const thesaurusStore          = useThesaurusStore ();
+    const countriesStore          = useCountriesStore ();
+    const realmConfStore          = useRealmConfStore();
+    const {$appRoutes:appRoutes } = useNuxtApp();
+    const selectedLocale = ref(locale.value);
+
+    const props = defineProps({
+        document    : { type:Object, default : undefined},
+        identifier  : { type:String, required:true}
+    });
+
+    const document = useKmStorage().cleanDocument({...props.document});
+
+</script>
