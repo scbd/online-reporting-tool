2b27a11934f2521de120bb065c5b31cbbc729f9b
Blaise Fonseca <blaise.fonseca@un.org>
2023-10-10 14:52:15 -0400

    Show facets on dashboard

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -5,208 +5,222 @@
       <!-- <template> -->
         <CCol md="12">
           <div class="alert alert-success" role="alert">
-            <h4 class="alert-heading">Recent Publish!</h4>
-            <p>
-              <strong>Gambia</strong> recently published their 7th National
-              Report.
-            </p>
-            <hr />
+            <div  v-if="publishedRecords?.length">
+                <h4 class="alert-heading">Recent Publish!</h4>
+                <p>
+                <strong>{{publishedRecords[0].country}}</strong> recently published {{ publishedRecords[0].type }}.
+                </p>
+                <hr />
+            </div>
             <p class="mb-0">
-              Are you a country representative responsible for 7NR submission,
-              <a href="#">click here</a> to begin
+              Are you a country representative responsible for National Report submission,
+              <km-link :to="appRoutes.NATIONAL_TARGETS_MY_COUNTRY">click here</km-link> to begin
             </p>
           </div>
         </CCol>
-        <CCol md="3" sm="6">
-          <div class="card">
-            <div class="card-header content-center text-white p-2 bg-success">
-              <span height="52" class="my-4">7th National Reports</span>
-            </div>
-            <div class="card-body row text-center">
-              <div class="col">
-                <div class="text-value-lg">63</div>
-                <div class="text-uppercase text-muted small">
-                  7th National Reports
+        <CCol md="3" sm="6">            
+            <km-link :to="appRoutes.NATIONAL_REPORTS_NR6" class="unset-anchor" target="_blank">
+                <div class="card">
+                    <div class="card-header content-center text-white p-2 bg-success">
+                    <span height="52" class="my-4">6th National Reports</span>
+                    </div>
+                    <div class="card-body row text-center">
+                    <div class="col">
+                        <div class="text-value-lg">{{nr6Count}}</div>
+                        <div class="text-uppercase text-muted small">
+                        6th National Reports
+                        </div>
+                    </div>
+                    <div
+                        class="c-vr text-value-lg"
+                        style="width: unset; background-color: unset"
+                    >
+                        By
+                    </div>
+                    <div class="col">
+                        <div class="text-value-lg">{{nr6CountryCount}}</div>
+                        <div class="text-uppercase text-muted small">Countries</div>
+                    </div>
+                    </div>
                 </div>
-              </div>
-              <div
-                class="c-vr text-value-lg"
-                style="width: unset; background-color: unset"
-              >
-                By
-              </div>
-              <div class="col">
-                <div class="text-value-lg">63</div>
-                <div class="text-uppercase text-muted small">Countries</div>
-              </div>
-            </div>
-          </div>
+            </km-link>
         </CCol>
         <CCol md="3" sm="6">
-          <div class="card">
-            <div class="card-header content-center text-white p-2 bg-success">
-              <span height="52" class="my-4">NBSAP targets</span>
-            </div>
-            <div class="card-body row text-center">
-              <div class="col">
-                <div class="text-value-lg">765</div>
-                <div class="text-uppercase text-muted small">
-                  National Targets
+            <km-link :to="appRoutes.NATIONAL_REPORTS_NR7" class="unset-anchor">
+                <div class="card">
+                    <div class="card-header content-center text-white p-2 bg-success">
+                    <span height="52" class="my-4">7th National Reports</span>
+                    </div>
+                    <div class="card-body row text-center">
+                    <div class="col">
+                        <div class="text-value-lg">{{nr7Count}}</div>
+                        <div class="text-uppercase text-muted small">
+                        7th National Reports
+                        </div>
+                    </div>
+                    <div
+                        class="c-vr text-value-lg"
+                        style="width: unset; background-color: unset"
+                    >
+                        By
+                    </div>
+                    <div class="col">
+                        <div class="text-value-lg">{{nr7CountryCount}}</div>
+                        <div class="text-uppercase text-muted small">Countries</div>
+                    </div>
+                    </div>
                 </div>
-              </div>
-              <div
-                class="c-vr text-value-lg"
-                style="width: unset; background-color: unset"
-              >
-                By
-              </div>
-              <div class="col">
-                <div class="text-value-lg">63</div>
-                <div class="text-uppercase text-muted small">Countries</div>
-              </div>
-            </div>
-          </div>
+            </km-link>
         </CCol>
         <CCol md="3" sm="6">
-          <div class="card">
-            <div class="card-header content-center text-white p-2 bg-success">
-              <span height="52" class="my-4">Assessed Targets</span>
-            </div>
-            <div class="card-body row text-center">
-              <div class="col">
-                <div class="text-value-lg">452</div>
-                <div class="text-uppercase text-muted small">
-                  Targets reviewed
+            <km-link :to="appRoutes.NATIONAL_TARGETS" class="unset-anchor">
+                <div class="card">
+                    <div class="card-header content-center text-white p-2 bg-success">
+                    <span height="52" class="my-4">National targets</span>
+                    </div>
+                    <div class="card-body row text-center">
+                    <div class="col">
+                        <div class="text-value-lg">{{nr7TargetCount}}</div>
+                        <div class="text-uppercase text-muted small">
+                        National Targets
+                        </div>
+                    </div>
+                    <div
+                        class="c-vr text-value-lg"
+                        style="width: unset; background-color: unset"
+                    >
+                        By
+                    </div>
+                    <div class="col">
+                        <div class="text-value-lg">{{ nr7TargetCountryCount }}</div>
+                        <div class="text-uppercase text-muted small">Countries</div>
+                    </div>
+                    </div>
                 </div>
-              </div>
-              <div
-                class="c-vr text-value-lg"
-                style="width: unset; background-color: unset"
-              >
-                By
-              </div>
-              <div class="col">
-                <div class="text-value-lg">45</div>
-                <div class="text-uppercase text-muted small">Countries</div>
-              </div>
-            </div>
-          </div>
+            </km-link>
         </CCol>
         <CCol md="3" sm="6">
-          <div class="card">
-            <div class="card-header content-center text-white p-2 bg-success">
-              <span height="52" class="my-4">Some other Number</span>
-            </div>
-            <div class="card-body row text-center">
-              <div class="col">
-                <div class="text-value-lg">25</div>
-                <div class="text-uppercase text-muted small">test</div>
-              </div>
-              <div
-                class="c-vr text-value-lg"
-                style="width: unset; background-color: unset"
-              >
-                By
-              </div>
-              <div class="col">
-                <div class="text-value-lg">3</div>
-                <div class="text-uppercase text-muted small">Countries</div>
-              </div>
-            </div>
-          </div>
+
+            <km-link :to="appRoutes.NATIONAL_REPORTS" class="unset-anchor">
+                <div class="card">
+                    <div class="card-header content-center text-white p-2 bg-success">
+                    <span height="52" class="my-4">National Biodiversity Strategies and Action Plans (NBSAPs)</span>
+                    </div>
+                    <div class="card-body row text-center">
+                    <div class="col">
+                        <div class="text-value-lg">{{nrCount}}</div>
+                        <div class="text-uppercase text-muted small">
+                            NBSAPs
+                        </div>
+                    </div>
+                    <div
+                        class="c-vr text-value-lg"
+                        style="width: unset; background-color: unset"
+                    >
+                        By
+                    </div>
+                    <div class="col">
+                        <div class="text-value-lg">{{nrCountryCount}}</div>
+                        <div class="text-uppercase text-muted small">Countries</div>
+                    </div>
+                    </div>
+                </div>
+            </km-link>
         </CCol>
+        
       <!-- </template> -->
     </CRow>
-    <!-- <CRow>
+    <CRow class="mt-5">
       <CCol md="12">
         <CCard>
           <CCardHeader> Latest records </CCardHeader>
           <CCardBody>
-            <CDataTable
-              class="mb-0 table-outline"
-              hover
-              :items="tableItems"
-              :fields="tableFields"
-              head-color="light"
-              no-sorting
-            >
-            </CDataTable>
+            <CTable hover>
+                <CTableHead>
+                    <CTableRow>
+                    <CTableHeaderCell scope="col">#</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">Title</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">Country</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">Type</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">Published on</CTableHeaderCell>
+                    </CTableRow>
+                </CTableHead>
+                <CTableBody>
+                    <CTableRow v-for="(record, $index) in publishedRecords" :key="record.identifier">
+                        <CTableHeaderCell scope="row">{{ $index+1 }}</CTableHeaderCell>
+                        <CTableDataCell>{{record.title}}</CTableDataCell>
+                        <CTableDataCell>{{record.country}}</CTableDataCell>
+                        <CTableDataCell>{{record.type}}</CTableDataCell>
+                        <CTableDataCell>{{ formatDate(record.publishedOn) }}</CTableDataCell>
+                    </CTableRow>
+                </CTableBody>
+            </CTable>
           </CCardBody>
         </CCard>
       </CCol>
-    </CRow> -->
+    </CRow>
   </div>
 </template>
 
 <script setup>
 import UserProfileInfo  from "@/components/common/user-profile-info.vue"
+import { useRealmConfStore } from '@/stores/realmConf';
+import { facets } from '@/services/solr';
+import { KmLink } from "@/components/controls";
 
-      definePageMeta({
-        auth:false
-      })
-      const gbfTargets=null
-      const selected = 'Month'
-      const tableItems = [
-        {
-          country: 'Canada',
-          type: '7th National Report',
-          title: 'Canada : 7th National Report',
-          publishedOn: randomDate(
-            new Date(2022, 0, 10),
-            new Date()
-          ).toLocaleString(),
-        },
-        {
-          country: 'Canada',
-          type: 'NBSAP Target',
-          title: 'National Target 1',
-          publishedOn: randomDate(
-            new Date(2022, 0, 10),
-            new Date()
-          ).toLocaleString(),
-        },
-        {
-          country: 'Germany',
-          type: 'NBSAP Target',
-          title: 'National Target 1',
-          publishedOn: randomDate(
-            new Date(2022, 0, 10),
-            new Date()
-          ).toLocaleString(),
-        },
-        {
-          country: 'Ghana',
-          type: 'NBSAP Target',
-          title: 'National Target 1',
-          publishedOn: randomDate(
-            new Date(2022, 0, 10),
-            new Date()
-          ).toLocaleString(),
+
+        definePageMeta({
+            auth:false
+        })
+      
+        const realmConfStore  = useRealmConfStore();
+        const realmConf = realmConfStore.realmConf;
+
+        const { $appRoutes:appRoutes } = useNuxtApp();
+
+
+        const searchQuery = {
+            rows:10,
+            q : `_state_s: public AND 
+                    ((schema_s : (${SCHEMAS.NATIONAL_TARGET_7} ${SCHEMAS.NATIONAL_TARGET_7_MAPPING}) AND realm_ss:${realmConf.realm}) OR 
+                    (schema_s : (${SCHEMAS.NATIONAL_REPORT_6} ${SCHEMAS.NATIONAL_REPORT}) AND realm_ss:${realmConf.realm.replace('ORT', 'CHM')}))`,
+            facet: true,
+            'facet.field': ['schema_s', 'government_EN_s'],
+            'facet.pivot' : "government_EN_s,schema_s",
+            sort: "updatedDate_dt desc"
         }
-      ]
-      const tableFields = [
-        { key: 'country', label: 'Country', _classes: '' },
-        { key: 'type', label: 'Record Type', _classes: '' },
-        { key: 'title', label: 'Title', _classes: '' },
-        { key: 'publishedOn', label: 'Published on' },
-      ]
+        const searchFacets = await facets(searchQuery)
+        const countryFacets = searchFacets.facetPivot['government_EN_s,schema_s'];
 
-    function color(value) {
-      let $color
-      if (value <= 25) {
-        $color = 'info'
-      } else if (value > 25 && value <= 50) {
-        $color = 'success'
-      } else if (value > 50 && value <= 75) {
-        $color = 'warning'
-      } else if (value > 75 && value <= 100) {
-        $color = 'danger'
-      }
-      return $color
-    }
-    function randomDate(start, end) {
-      return new Date(
-        start.getTime() + Math.random() * (end.getTime() - start.getTime())
-      )
-    }
+
+        const nr7Count       = computed(()=>countryFacets?.reduce((prevVal, currVal)=> prevVal + (schemaCount(currVal, SCHEMAS.NATIONAL_REPORT_7)), 0))
+        const nr7TargetCount = computed(()=>countryFacets?.reduce((prevVal, currVal)=> prevVal + (schemaCount(currVal, SCHEMAS.NATIONAL_TARGET_7)), 0))
+        const nr6Count       = computed(()=>countryFacets?.reduce((prevVal, currVal)=> prevVal + (schemaCount(currVal, SCHEMAS.NATIONAL_REPORT_6)), 0))
+        const nrCount        = computed(()=>countryFacets?.reduce((prevVal, currVal)=> prevVal + (schemaCount(currVal, SCHEMAS.NATIONAL_REPORT)  ), 0))
+              
+        const nr7CountryCount       = computed(()=>countryFacets?.reduce((prevVal, currVal)=> prevVal + (schemaCount(currVal, SCHEMAS.NATIONAL_REPORT_7) > 0 ? 1 : 0), 0))
+        const nr7TargetCountryCount = computed(()=>countryFacets?.reduce((prevVal, currVal)=> prevVal + (schemaCount(currVal, SCHEMAS.NATIONAL_TARGET_7) > 0 ? 1 : 0), 0))
+        const nr6CountryCount       = computed(()=>countryFacets?.reduce((prevVal, currVal)=> prevVal + (schemaCount(currVal, SCHEMAS.NATIONAL_REPORT_6) > 0 ? 1 : 0), 0))
+        const nrCountryCount        = computed(()=>countryFacets?.reduce((prevVal, currVal)=> prevVal + (schemaCount(currVal, SCHEMAS.NATIONAL_REPORT)   > 0 ? 1 : 0), 0))
+      
+        const publishedRecords = computed(()=>{ 
+            return searchFacets.docs?.map(e=> {
+                return {
+                    country:e.government_EN_t, title: e.title_EN_t, type:e.schema_EN_t,
+                    identifier: e.identifier_s, publishedOn:e.submittedDate_dt,
+                    schema:e.schema_s
+                }
+            })
+        })
+
+        function schemaCount(currVal, schema){
+                return currVal.pivot.find(e=>e.value == schema)?.count||0
+        }
 </script>
+
+<style scoped>
+    .unset-anchor{
+        text-decoration: unset;
+        color: unset;
+    }
+</style>
