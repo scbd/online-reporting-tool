335239e3a6405e77a602e43b02f425df5d16269e
Blaise Fonseca <blaise.fonseca@un.org>
2023-10-17 12:06:16 -0400

    add scrollToTop hook

--- plugins/initialHooks.ts
+++ plugins/initialHooks.ts
@@ -7,5 +7,13 @@ export default defineNuxtPlugin((nuxtApp)=>{
         await realmConfStore.loadRealmConf();
 
     })
+
+    useNuxtApp().hook("page:finish", () => {
+        if (history.state.scroll) {
+          setTimeout(() => window.scrollTo(history.state.scroll), 0);
+        } else {
+          setTimeout(() => window.scrollTo(0, 0), 0);
+        }
+    });
 })
 
