23f0ea951945b0e497b49a9f731c49d7d1548307
Blaise Fonseca <blaise.fonseca@un.org>
2023-10-19 15:09:35 -0400

    Fix scroll behaviour

--- plugins/app-hooks.ts
+++ plugins/app-hooks.ts
@@ -1,19 +1,13 @@
 import { useRealmConfStore } from "@/stores/realmConf"
 
 export default defineNuxtPlugin((nuxtApp)=>{
+    
     nuxtApp.hook('app:beforeMount', async ()=>{
         
         const realmConfStore = useRealmConfStore();
         await realmConfStore.loadRealmConf();
 
     })
-
-    useNuxtApp().hook("page:finish", () => {
-        if (history.state.scroll) {
-          setTimeout(() => window.scrollTo(history.state.scroll), 0);
-        } else {
-          setTimeout(() => window.scrollTo(0, 0), 0);
-        }
-    });
+    
 })
 
