dc19f7d7f180751b217ab953aa6b0b6c6c0063be
Randy Houlahan <randy.houlahan@cbd.int>
2024-02-21 12:27:21 -0500

    save

--- components/layout/AppBreadcrumb.vue
+++ components/layout/AppBreadcrumb.vue
@@ -1,6 +1,8 @@
-<template>{{ currentRoute }} - {{ currentRoute2 }}
+<template>
+<!-- {{ currentRoute }} <br> -->
+<!-- - <br>{{ currentRoute2 }} -->
   <CBreadcrumb class="d-md-down-none me-auto mb-0"> 
-    <NuxtLink v-for="item in breadcrumbs" class="breadcrumb-item"
+    <NuxtLink v-for="item in crumbs" class="breadcrumb-item"
       :key="item"
       :href="item.active ? '' : item.to"
       :class="{'active' : item.active}">{{ item.text }}</NuxtLink>
@@ -11,23 +13,29 @@
 import { onMounted, ref } from 'vue'
 import { capitalCase } from 'change-case';
 import { languages } from '@/app-data/languages'
-import { useRoute as useRouteVue } from 'vue-router' 
-import { useRoute } from '#imports';
+const router = useRouter()
+// import { useRoute as useRouteVue } from 'vue-router' 
+// import { useRoute } from '#imports';
 //TODO: verify when nuxt issue is fixed, temp use useRoute directly from vue instead of nuxt
 
     const localePath  = useLocalePath();
-    const route = useRoute();
-
+   // const route = useRoute();
+    const crumbs =ref([]);
     function makeCrumbs (){ //eslint-disable-line
+      
+       const route = router.currentRoute.value
+// console.log(route.href, localePath())
+// console.log('===========', route?.href)
+// console.log('===========', useRouteVue()?.href)
 
-    //   const route = useRoute();
-console.log(route.href, useRouteVue())
-      const crumbs    = [
+crumbs.value= [
         { text:'CHM', to:'https://chm.cbd.int' },
       ];
       if(route){
-        
-        crumbs.push({ text:'ORT', to: localePath('/dashboard'), active: '/dashboard' === route.fullPath });
+        console.log('=========== route.href', route.href)
+        console.log('router',router)
+        console.log('router.currentRoute',router.currentRoute.value.href)
+        crumbs.value.push({ text:'ORT', to: localePath('/dashboard'), active: '/dashboard' === route.fullPath });
         const {breadcrumbs : pageBreadcrumbsConf } = route.meta;
         
 
@@ -55,17 +63,24 @@ console.log(route.href, useRouteVue())
             
             const crumb = { text, to, active: to === fullPath, };
 
-            crumbs.push(crumb);
+            crumbs.value.push(crumb);
           }
         }
       }
+      console.log('crumbs',crumbs.value)
+      console.log('router.currentRoute',router.currentRoute)
+      
+
       return crumbs;
     }
-    const breadcrumbs = computed(()=>{
-      return makeCrumbs()
-    })
-
-    const currentRoute = computed(()=>route.fullPath)
-    const currentRoute2 = computed(()=>useRouteVue()?.fullPath)
-      
+    const breadcrumbs = computed(makeCrumbs)
+    makeCrumbs ()
+    // const currentRoute = computed(()=>route.fullPath)
+    // const currentRoute2 = computed(()=>useRouteVue()?.fullPath)
+    router.beforeEach(() => {
+      makeCrumbs ()
+        })
+    router.afterEach(() => {
+      makeCrumbs ()
+        })
 </script>
