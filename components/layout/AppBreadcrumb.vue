3bd606ab35b197345d97cb14d54732c0f610fd5b
Blaise Fonseca <blaise.fonseca@un.org>
2024-02-22 12:56:30 -0500

    fix breadcrumbs update issue

--- components/layout/AppBreadcrumb.vue
+++ components/layout/AppBreadcrumb.vue
@@ -7,58 +7,63 @@
   </CBreadcrumb>
 </template>
 
-<script setup>
+<script>
 import { onMounted, ref } from 'vue'
 import { capitalCase } from 'change-case';
 import { languages } from '@/app-data/languages'
 import { useRoute } from 'vue-router' 
 //TODO: verify when nuxt issue is fixed, temp use useRoute directly from vue instead of nuxt
 
-    const localePath  = useLocalePath();
-    const route = useRoute();
+export default defineComponent({
+    setup(props) {
+        const route = useRoute();
+        const localePath  = useLocalePath();
 
-    function makeCrumbs (){ //eslint-disable-line
+        const breadcrumbs = computed(()=>{
+            const crumbs    = [
+                { text:'CHM', to:'https://chm.cbd.int' },
+            ];
+            if(route){
+                
+                crumbs.push({ text:'ORT', to: localePath('/dashboard'), active: '/dashboard' === route.fullPath });
+                const {breadcrumbs : pageBreadcrumbsConf } = route.meta;
+                
 
-      const route = useRoute();
-      const {breadcrumbs : pageBreadcrumbsConf } = route.meta;
+                let { path }  = route.matched[0] || { path: '/' };
+                path          = path.replace('()', '').replace(/\?$/, '');
+                const pathSplit = () => path.split('/').splice(1);
+                if (!path) return [];
 
-      const crumbs    = [
-        { text:'CHM', to:'https:chm.cbd.int' },
-        { text:'ORT', to: localePath('/dashboard'), active: '/dashboard' === route.fullPath }
-      ];
-      if(route){
-        let { path }  = route.matched[0] || { path: '/' };
-        path          = path.replace('()', '').replace(/\?$/, '');
-        const pathSplit = () => path.split('/').splice(1);
-        if (!path) return [];
+                const skipParams = pageBreadcrumbsConf?.skip?.map(e=>`:${e}`)
+                for (const [index, routeName] of pathSplit().entries()) { // eslint-disable-line
+                    if (!routeName) continue; // eslint-disable-line no-continue
+                    if(!Object.keys(languages).includes(routeName)){
 
-        const skipParams = pageBreadcrumbsConf?.skip?.map(e=>`:${e}`)
-        for (const [index, routeName] of pathSplit().entries()) { // eslint-disable-line
-          if (!routeName) continue; // eslint-disable-line no-continue
-          if(!Object.keys(languages).includes(routeName)){
+                        if(skipParams?.includes(routeName))
+                        continue;
 
-            if(skipParams?.includes(routeName))
-              continue;
+                        const paths = pathSplit().filter(e=>!skipParams?.includes(e))
+                        let fullPath = route.fullPath;
+                        
+                        pageBreadcrumbsConf?.skip?.forEach(element => {
+                        fullPath = fullPath.replace(`/${route.params[element]}`, '');
+                        });
+                        const text  = capitalCase(routeName);
+                        const to    = index ? `/${paths.splice(0, index + 1).join('/')}` : `/${routeName}`;
+                        
+                        const crumb = { text, to, active: to === fullPath, };
 
-            const paths = pathSplit().filter(e=>!skipParams?.includes(e))
-            let fullPath = route.fullPath;
-            
-            pageBreadcrumbsConf?.skip?.forEach(element => {
-              fullPath = fullPath.replace(`/${route.params[element]}`, '');
-            });
-            const text  = capitalCase(routeName);
-            const to    = index ? `/${paths.splice(0, index + 1).join('/')}` : `/${routeName}`;
-            
-            const crumb = { text, to, active: to === fullPath, };
+                        crumbs.push(crumb);
+                    }
+                }
+            }
+            return crumbs;
+        })
 
-            crumbs.push(crumb);
-          }
+        return {
+            breadcrumbs
         }
-      }
-      return crumbs;
     }
-    const breadcrumbs = computed(()=>{
-      return makeCrumbs()
-    })
-      
+})
+
 </script>
