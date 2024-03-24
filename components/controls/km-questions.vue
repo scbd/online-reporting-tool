c956c59df22e9e45831ee99ea7f8832372a411fb
Blaise Fonseca <blaise.fonseca@un.org>
2024-03-24 16:14:36 -0400

    Recursive question component

---
+++ components/controls/km-questions.vue
@@ -0,0 +1,67 @@
+<template>
+    <div  v-if="answersInitialized" >
+    <div class="mb-3" v-for="question in questions" :key="question.key">           
+        <div v-if="!question.questions?.length">
+            <km-question :question="question" v-model="answers[question.key]" @update:modelValue="onAnswer"></km-question>
+        </div>
+        <div v-if="question.questions?.length">
+            <!-- <CCard>
+                <CCardHeader> -->
+                    <km-form-group>
+                        <template #caption>
+                            {{question?.number}} {{question?.title}}
+                        </template>
+                    </km-form-group>
+                <!-- </CCardHeader>
+                <CCardBody> -->
+                    <km-questions class="ms-3" :subQuestion="true" :questions="question.questions" v-model="answers[question.key]" @update:modelValue="onAnswer"></km-questions>  
+                <!-- </CCardBody>
+            </CCard>           -->
+        </div>
+    </div></div>
+</template>
+
+<i18n src="@/i18n/dist/components/controls/km-questions.json"></i18n>
+
+<script setup lang="ts">
+    import { flattenObject } from "~/utils/helpers";
+const attrs = useAttrs();
+    const model = defineModel<Array<Object>>({required:true});
+    const props = defineProps({
+        questions : {type:Object as PropType<Question[]>}
+    });
+
+    const answersInitialized = ref(false);
+    const answers = ref({});
+
+    const  onAnswer = function(){
+        const newVal = flattenObject(answers.value);     
+        model.value = newVal;
+    }
+
+    const answerArrayToHierarchy = function (questions, answers){
+        const hierarchy = {};
+        for (let i = 0; i < questions.length; i++) {
+            const question = questions[i];
+            if(!question.questions){
+                hierarchy[question.key] = answers[question.key];
+            }
+            else{
+                hierarchy[question.key] = answerArrayToHierarchy(question.questions, answers);
+            }
+        }
+        return hierarchy;
+    }
+
+    onMounted(()=>{       
+        answers.value = answerArrayToHierarchy(props.questions, model.value||{});
+        answersInitialized.value = true;
+    })
+
+
+
+</script>
+
+<style>
+
+</style>
