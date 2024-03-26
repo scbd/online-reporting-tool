85dfd5b15530d28111c62368a36ac70a861a69a7
Blaise Fonseca <blaise.fonseca@un.org>
2024-03-26 13:39:29 -0400

    Add identifier to other national indicators

--- types/schemas/ENationalTarget7.ts
+++ types/schemas/ENationalTarget7.ts
@@ -2,26 +2,26 @@
 interface ENationalTarget7 {
     header                         : EHeader;
     government                     : ETerm;
-    title                          : typeof ELString;
-    description                    : typeof ELString;
+    title                          : ELString;
+    description                    : ELString;
     sequence                       : number;
     globalGoalAlignment            : ETerm[];
     globalTargetAlignment          : TargetAlignment[];
-    degreeOfAlignmentInfo          : typeof ELString;
+    degreeOfAlignmentInfo          : ELString;
     implementingConsiderations     : ETerm[];
-    implementingConsiderationsInfo : typeof ELString;
-    mainPolicyOfMeasureOrActionInfo: typeof ELString;
+    implementingConsiderationsInfo : ELString;
+    mainPolicyOfMeasureOrActionInfo: ELString;
     headlineIndicators             : ETerm[];
     binaryIndicators               : ETerm[];
     componentIndicators            : ETerm[];
     complementaryIndicators        : ETerm[];
-    otherNationalIndicators        : typeof ELString[];
-    nonStateActorCommitmentInfo    : typeof ELString;
+    otherNationalIndicators        : NationalIndicators[];
+    nonStateActorCommitmentInfo    : ELString;
     hasNonStateActors              : boolean;
-    nonStateActorsInfo             : typeof ELString;
+    nonStateActorsInfo             : ELString;
     additionalImplementation       : LTerm;
-    additionalImplementationInfo   : typeof ELString;
-    additionalInformation          : typeof ELString;
+    additionalImplementationInfo   : ELString;
+    additionalInformation          : ELString;
     additionalDocuments            : ELink[];
     notes                          : string;
 }
@@ -32,4 +32,10 @@ interface LTerm extends ETerm {
 
 interface TargetAlignment extends ETerm {
     degreeOfAlignment: ETerm;
+}
+
+interface NationalIndicators
+{
+    identifier  : string;
+    value       : ELString;
 }
