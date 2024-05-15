32a98e1723a97a566a03edf044fe214886706b79
Blaise Fonseca <blaise.fonseca@un.org>
2024-05-14 22:20:08 -0400

    National target overview onboarding.

--- components/common/on-boarding.vue
+++ components/common/on-boarding.vue
@@ -1,13 +1,17 @@
 <template>
+    <div class="col-12" style="height:0px" id="tourWelcome">
+        <!-- Dummy div to show a welcome message on each screen -->
+    </div>
     <div  v-if="steps?.length && isReady">
         <Teleport to="#takeTourTeleport" >
+            <Transition>
             <a class="text-dark me-md-1" rel="noopener" href="#" @click.prevent="startTour()">
                 <font-awesome-icon icon="person-chalkboard" />
                 <span class="ms-1">Take a tour</span>
-            </a>
+            </a></Transition>
         </Teleport>
     </div>
-    <VOnboardingWrapper ref="wrapper" :steps="steps" />
+    <VOnboardingWrapper ref="wrapper" :steps="steps" @finish="onTourEnd" @exit="onTourEnd" />
 </template>
 
 <script setup lang="ts">
@@ -18,17 +22,23 @@
         steps : {type:Array, required:true},
         teleportTo: {type:String}
     })
+    const emit = defineEmits(['onTourStart','onTourEnd'])
 
     const isReady = ref(false);    
     const wrapper = ref(null)
     const { start, goToStep, finish } = useVOnboarding(wrapper);
 
     function startTour(){
+        emit('onTourStart')
         start();
     }
 
+    function onTourEnd(){
+        emit('onTourEnd')
+    }
+
     onMounted(()=>{
-        sleep(2000).then(()=>isReady.value = true);
+        nextTick(()=>isReady.value = true);
     })
 </script>
 
