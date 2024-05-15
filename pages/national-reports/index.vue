6f1e3ecd34c6b5e6b917696d01b166f7e7bbcbce
Blaise Fonseca <blaise.fonseca@un.org>
2024-05-15 14:58:19 -0400

    NR7 search list page

--- pages/national-reports/index.vue
+++ pages/national-reports/index.vue
@@ -1,21 +1,37 @@
 <template>
-    <CCard>
-      <CCardHeader>
-        <slot name="header"> <CIcon name="cil-grid" /> {{ caption }} </slot>
-      </CCardHeader>
-      <CCardBody>
-        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-          <NuxtLink :to="appRoutes.NATIONAL_REPORTS_NR7_MY_COUNTRY_EDIT_OVERVIEW">
-            <CButton color="secondary" size="sm">
-              <CIcon icon="addthis"/>All national reports
-            </CButton>
-          </NuxtLink>
-        </div>
-        List of reports
-      </CCardBody>
-    </CCard>
-  </template>
-  <!-- <i18n src="../i18n/dist/pages/index.json"></i18n> -->
-  <script>
- 
-  </script>
+  <CCard>
+    <CCardHeader>
+      <slot name="header"> <CIcon name="cil-grid" /> {{ t('allNationalReports') }} </slot>
+    </CCardHeader>
+    <CCardBody>
+      <div class="d-grid gap-2 d-md-flex justify-content-md-end mb-3">
+        <NuxtLink :to="appRoutes.NATIONAL_REPORTS_NR7_MY_COUNTRY_EDIT_OVERVIEW" class="btn btn-secondary">
+            <font-awesome-icon icon="fa-plus"></font-awesome-icon> {{ t('myCountryNr7') }}
+        </NuxtLink>
+      </div>
+
+      <km-suspense>
+          <nr7-list></nr7-list>
+      </km-suspense>
+    </CCardBody>
+  </CCard>
+</template>
+
+<i18n  src="~/i18n/dist/pages/national-targets/index.json"></i18n>
+<script setup lang="ts">
+
+import { SCHEMAS, ROLES } from '@/utils';
+import { KmSuspense } from "@/components/controls";
+
+  const { $appRoutes:appRoutes } = useNuxtApp();
+  const { t }  = useI18n();
+  definePageMeta({
+      auth:false,
+      schema:SCHEMAS.NATIONAL_TARGET_7,
+      roles:[...ROLES.ALL_NATIONAL_USERS],
+  })
+  const navigateToPage = async (url:string)=>{
+    await useNavigateAppTo(url);
+  }
+  
+</script>
