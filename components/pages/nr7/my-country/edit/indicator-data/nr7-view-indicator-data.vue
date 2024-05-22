e30c082c13b99f785fe8949ed67ce424dde26edf
Blaise Fonseca <blaise.fonseca@un.org>
2024-05-22 12:23:49 +0300

    fix data view

--- components/pages/nr7/my-country/edit/indicator-data/nr7-view-indicator-data.vue
+++ components/pages/nr7/my-country/edit/indicator-data/nr7-view-indicator-data.vue
@@ -1,25 +1,9 @@
 <template>
-    <!-- <CCard> -->
-        <!-- <CCardBody v-if="indicatorData"> -->
             <km-form-group v-if="indicatorData.sourceOfData" :caption="t('sourceOfData')">
                 <km-value>{{ t(indicatorData.sourceOfData) }}</km-value>
             </km-form-group>
             <km-form-group caption="Data"  v-if="indicatorData.data">
                 <div class="w-100" style="overflow: auto;">
-                    <!-- <CTable responsive bordered>
-                        <CTableBody>
-                            <CTableRow>
-                                <CTableHeaderCell scope="col">{{t('year')}}</CTableHeaderCell>
-                                <CTableDataCell v-for="unit in indicatorData.data" :key="unit">{{ unit.year }}
-                                </CTableDataCell>
-                            </CTableRow>
-                            <CTableRow>
-                                <CTableHeaderCell scope="row">{{t('value')}}</CTableHeaderCell>
-                                <CTableDataCell v-for="unit in indicatorData.data" :key="unit">{{ unit.value }}
-                                </CTableDataCell>
-                            </CTableRow>
-                        </CTableBody>
-                    </CTable> -->
                     <table class="table responsive table-bordered">
                         <tbody>
                         <tr>
@@ -49,24 +33,21 @@
 
             <km-form-group v-if="indicatorData.globalDataSources" :caption="t('globalSourceOfData')">
                 <km-value v-for="source in indicatorData.globalDataSources" :key="source" class="mt-1">
-                    <a :href="source.link" target="_blank">{{ source.title }}</a>
+                    <a :href="source.url" target="_blank">{{ source.name }}</a>
                 </km-value>
             </km-form-group>
             <km-form-group v-if="indicatorData.globalIndicatorProviders" :caption="t('globalIndicatorProviders')">
                 <km-value v-for="source in indicatorData.globalIndicatorProviders" :key="source" class="mt-1">
-                    <a :href="source.link" target="_blank">{{ source.title }}</a>
+                    <a :href="source.url" target="_blank">{{ source.name }}</a>
                 </km-value>
             </km-form-group>
-            <!-- <km-form-group v-if="indicatorData.globalDescription">
-                <label class="form-check-label">globalDescription</label>
+            <km-form-group v-if="indicatorData.globalDescription" :caption="t('description')">
                 <km-value>{{ indicatorData.globalDescription }}</km-value>
-            </km-form-group> -->
+            </km-form-group>
             <km-form-group v-if="indicatorData.comments" class="mt-1" :caption="t('comments')">
                 <km-lstring-value type="html" :value="indicatorData.comments"
                     :locale="indicatorData.languages"></km-lstring-value>
             </km-form-group>
-        <!-- </CCardBody> -->
-    <!-- </CCard> -->
 </template>
 <i18n src="@/i18n/dist/components/pages/nr7/my-country/edit/indicator-data/nr7-view-indicator-data.json"></i18n>
 <script setup lang="ts">
