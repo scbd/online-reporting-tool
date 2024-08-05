be156141df3fb22fe449b5bbb6139dfd5338a216
Blaise Fonseca <blaise.fonseca@un.org>
2024-08-05 14:06:55 -0400

    handle custom routes

--- pages/database/[identifier].vue
+++ pages/database/[identifier].vue
@@ -19,7 +19,19 @@
     })
 
     const route      = useRoute();
-    const identifier = route.params.identifier;
+    const identifier = computed(()=>{
+        
+        const identifier = route.params.identifier;
+
+        const uidRegex = /(ORT(\-DEV)?)\-([A-Z]{2,3}[0-9])\-([A-Z]{2})\-([0-9]+)(\-[0-9]+)?/i;
+        const matchedUId = identifier.match(uidRegex);
+        
+        if(matchedUId?.length > 4){
+            return matchedUId[5];
+        }
+
+        return identifier;
+    });
 
 </script>
 
