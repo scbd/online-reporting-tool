55b1f877afd92fc29758cc8b1c0928eb88a5b6be
Blaise Fonseca <blaise.fonseca@un.org>
2024-08-30 14:14:56 -0400

    Subscription and user setting api

---
+++ api/subscription.ts
@@ -0,0 +1,43 @@
+import type { MongoQuery } from '~/types/api-schemas/mongo-query';
+import ApiBase, { tryCastToApiError } from './api-base';
+
+export default class SubscriptionApi extends ApiBase
+{
+    constructor(options) {
+        super(options);
+    }
+        
+    async list(query:Object, options : MongoQuery) {  
+        
+        const { count, length, skip, sort } = options || {};
+        const httpQuery = {
+            q : JSON.stringify(query||{}),
+            l : length   ||25,
+            s : sort,
+            sk: skip     ||0,
+            c : count
+        };
+
+        const data =  await useAPIFetch(`/api/v2016/me/subscriptions`,  { method:'get', query:httpQuery })                
+        return data;
+    }
+
+    async get(id:String)  {
+
+        const data =  await useAPIFetch(`/api/v2016/me/subscriptions/${encodeURIComponent(id)}`,  { method:'get' })                
+        return data;
+    }
+
+    async add(body:Subscription) {
+        return useAPIFetch("/api/v2016/me/subscriptions/", {method:'POST', body})
+    }
+
+    async update(id:String, body:Subscription) {
+        return useAPIFetch("/api/v2016/me/subscriptions/" + id , {method:'PUT', body})
+    }
+
+    async delete(id:string) {
+        return useAPIFetch(`/api/v2016/me/subscriptions/${id}`, {method:'DELETE' });
+
+    }
+}
