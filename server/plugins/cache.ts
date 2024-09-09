42458720397dfb45686e9945bd5742e8b05999db
Blaise Fonseca <blaise.fonseca@un.org>
2024-09-09 09:59:51 -0400

    Allow iframe embed if query has embed

--- server/plugins/cache.ts
+++ server/plugins/cache.ts
@@ -1,7 +1,11 @@
 import { RenderResponse } from "nitropack"
 
 export default defineNitroPlugin((nitroApp) => {
-    nitroApp.hooks.hook('render:response', (res: RenderResponse) => {
+    nitroApp.hooks.hook('render:response', (res: RenderResponse, req) => {
         res.headers['cache-control'] = `no-cache`
+
+        const query = getQuery(req.event)
+        if(query?.embed == 'true')
+            res.headers['X-Frame-Options'] = 'ALLOW';
     })
 })
