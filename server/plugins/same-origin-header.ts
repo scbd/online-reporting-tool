8beb657596167dfb44c4472870a55363550fb1a4
Blaise Fonseca <blaise.fonseca@un.org>
2024-09-16 10:24:49 -0400

    extract hook to individual file

--- server/plugins/same-origin-header.ts
+++ server/plugins/same-origin-header.ts
@@ -2,6 +2,8 @@ import { RenderResponse } from "nitropack"
 
 export default defineNitroPlugin((nitroApp) => {
     nitroApp.hooks.hook('render:response', (res: RenderResponse, req) => {
-        res.headers['cache-control'] = `no-cache`
+        const query = getQuery(req.event)
+        if(query?.embed == 'true')
+            res.headers['X-Frame-Options'] = 'ALLOW';
     })
 })
