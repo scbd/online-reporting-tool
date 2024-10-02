9457210f05e66d9582377b3297a0c1e7e53699d3
Blaise Fonseca <blaise.fonseca@un.org>
2024-10-02 18:03:40 -0400

    Nbsap edit/view forms

---
+++ pages/nbsaps/my-country/[identifier]/edit.vue
@@ -0,0 +1,22 @@
+<template>
+  <km-suspense>
+    <edit-nbsap></edit-nbsap>
+  </km-suspense>
+</template>
+  
+<script setup lang="ts">
+
+    const schema      = useGetRealmSchema(SCHEMAS.NATIONAL_NBSAP)
+    const { locale }  = useI18n();
+    const route       = useRoute();
+    definePageMeta({
+        auth:true,
+        schema:SCHEMAS.NATIONAL_NBSAP,
+        roles:[...ROLES.ALL_NATIONAL_USERS],
+        breadcrumbs : {
+          skip : ['identifier']
+        }
+    })
+    route.meta.breadcrumbs.replaceCrumbs = [ {'nbsaps' : lstring(schema?.title, locale)} ]
+</script>
+  
---
+++ pages/nbsaps/my-country/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <km-suspense>
+      <my-country-nbsaps></my-country-nbsaps>
+  </km-suspense>
+  
+</template>
+
+<script setup lang="ts">
+  import MyCountryNbsaps from "@/components/pages/nbsaps/my-country/index.vue";
+  import { SCHEMAS, ROLES } from '@/utils';
+
+    const schema      = useGetRealmSchema(SCHEMAS.NATIONAL_NBSAP)
+    const { locale }  = useI18n();
+    const route       = useRoute();
+
+    definePageMeta({
+        auth:true,
+        schema:SCHEMAS.NATIONAL_NBSAP,
+        roles:[...ROLES.ALL_NATIONAL_USERS],
+        breadcrumbs : {}
+    });
+
+    route.meta.breadcrumbs.replaceCrumb = [ {'nbsaps' : lstring(schema?.title, locale)} ];
+    
+</script>
