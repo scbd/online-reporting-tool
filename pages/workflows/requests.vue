6d6c959db67883d81f2979a87b936aa6667613b6
Blaise Fonseca <blaise.fonseca@un.org>
2024-10-04 13:46:12 -0400

    Workflow request control for draft views

---
+++ pages/workflows/requests.vue
@@ -0,0 +1,16 @@
+<template>
+    <CCard>
+        <CCardHeader>{{t('workflowRequestsTitle')}}</CCardHeader>
+        <CCardBody>
+            <workflow-requests></workflow-requests>
+        </CCardBody>
+    </CCard>
+</template>
+<i18n src="@/i18n/dist/pages/workflows/requests.json"></i18n>
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
