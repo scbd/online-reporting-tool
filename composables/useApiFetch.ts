b767ad1acbc418029f11000644175e6332c90d63
Blaise Fonseca <blaise.fonseca@un.org>
2024-10-17 14:53:06 -0400

    Nbsap analyzer (#114)

--- composables/useApiFetch.ts
+++ composables/useApiFetch.ts
@@ -15,7 +15,7 @@ export default class ApiError extends Error {
 export const useAPIFetch: useFetchType = async (path, options = {}) => {
     //TODO: find why useFetch is returning cache response even when 
     //      there is network call and for POST method
-    const key   = hash({...options, requestedOn:new Date().getTime().toString()})
+    const key   = hash({...options, path, requestedOn:new Date().getTime().toString()})
     options.key = key;//generate unique key to avoid caching
     options.cache = 'no-cache';
     
