edfc98521a9f977ddad40ec7286645340cff4373
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2024-10-25 14:23:24 +0500

    update footer component

--- components/layout/AppFooter.vue
+++ components/layout/AppFooter.vue
@@ -2,15 +2,16 @@
   <cbd-footer></cbd-footer>
   <CFooter>
     <div>
-      <a href="https://ort.cbd.int" target="_blank">ORT</a>
+      <a href="https://ort.cbd.int" target="_blank">{{ t('ort') }}</a>
        &nbsp;      
-      <a href="https://chm.cbd.int" target="_blank">CHM</a>
+      <a href="https://chm.cbd.int" target="_blank">{{ t('chm') }}</a>
     </div>
     <div>
-      <span class="ms-1">&copy; {{ new Date().getFullYear() }} <a href="https://cbd.int" target="_blank">SCBD</a>.</span>
+      <span class="ms-1">&copy; {{ new Date().getFullYear() }} <a href="https://cbd.int" target="_blank">{{ t('scbd')}}</a>.</span>
     </div>
   </CFooter>
 </template>
+<i18n src="@/i18n/dist/components/layout/AppFooter.json"></i18n>
 
 <script>
 import cbdFooter from './cbd/footer.vue'
