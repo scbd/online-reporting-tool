330da3c67405e2820276536c5d8ba6083576b1ab
Blaise Fonseca <blaise.fonseca@un.org>
2025-05-28 21:35:03 -0400

    Adds srcset to attachments images
    Adds `srcset` attribute to attachment images in KML string values.
    
    This enhancement ensures that images are appropriately sized for different screen resolutions, improving the user experience and optimizing bandwidth usage.

--- components/controls/view/KmLstringValue.vue
+++ components/controls/view/KmLstringValue.vue
@@ -26,6 +26,7 @@
 //@ts-nocheck
     import { direction, lstringLocale, lstring } from '@/utils';
     import { useThesaurusStore }    from '@/stores/thesaurus';
+    import $ from 'jquery';
 
     const props = defineProps({
         value  : {type:Object, required:true },
@@ -34,15 +35,50 @@
     })
     const { value, locale, type } = toRefs(props);    
     const thesaurusStore    = useThesaurusStore ();
-
+    
     const valueLocale   = computed(()=>lstringLocale(value.value, locale.value));
-    const valueLstring  = computed(()=>lstring(value.value, locale.value));
+    const valueLstring  = computed(()=>{
+        const localeValue = lstring(value.value, locale.value);
+        if(type.value=='html')
+            return processImageSrcset(localeValue);
+
+        return localeValue;
+    });
 
     const getTerm = computed(()=>{
-        thesaurusStore.loadTerm(`lang-${valueLocale.value}`)
         return thesaurusStore.getTerm(`lang-${valueLocale.value}`)||{};   
     })
     
+    function processImageSrcset(localeValue:string){
+
+        // ATTACHMENTS_API_REGEX
+        const htmlContent = $('<div>').html(localeValue);
+        const newHtml = htmlContent.find('img')
+            .filter((index, element)=>!element.srcset)
+            .filter((index, element)=>element.src && element.src.match(ATTACHMENTS_API_REGEX))
+            .map((index, element)=>{
+                let srcSets = Object.values({
+                                'xs': 320,
+                                'sm': 640,
+                                'md': 768,
+                                'lg': 1024,
+                                'xl': 1280,
+                                'xxl': 1536
+                            });
+                element.sizes  ="(max-width: 640px) 320px, (max-width: 768px) 640px, (max-width: 1024px) 768px, (max-width: 1280px) 1024px, 1280px" 
+                element.srcset = srcSets.map(size=>{
+                                    const src = new URL(element.src);
+                                    src.searchParams.set('width', size);
+                                    return `${src.toString()} ${size}w`      
+                                }).join(', ')
+            });
+            
+        return htmlContent.html();
+            
+    }
+
+    await thesaurusStore.loadTerm(`lang-${valueLocale.value}`)
+
 </script>
 
 <style>
