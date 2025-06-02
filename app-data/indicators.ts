f3d6f7b929284e44ff5f28f56f6cd615a08e6ce5
Blaise Fonseca <blaise.fonseca@un.org>
2025-06-02 14:40:29 -0400

    Indicator data to download data excel

---
+++ app-data/indicators.ts
@@ -0,0 +1,24 @@
+import type { IndicatorMapping } from '~/types/controls/indicator-mapping';
+
+export const IndicatorsMappingData : IndicatorMapping[] = [
+    {
+        "identifier": "GBF-INDICATOR-A.1",
+        "code": "A.1",
+        "title": "A.1 Red List of Ecosystems"
+    },
+    {
+        "identifier": "GBF-INDICATOR-259",
+        "code": "4.CT.1",
+        "title": "4.CT.1 Number of (a) plant and (b) animal genetic resources for food and agriculture secured in either medium- or long-term conservation facilities "
+    },
+    {
+        "identifier": "GBF-INDICATOR-4.CT.4",
+        "code": "4.CT.4",
+        "title": "4.CT.4 Proportion of local breeds classified as being at risk of extinction "
+    },
+    {
+        "identifier": "GBF-INDICATOR-114",
+        "code": "8.CT.2",
+        "title": "8.CT.2 Bioclimatic Ecosystem Resilience Index"
+    }
+]
