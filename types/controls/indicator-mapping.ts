8a8a40f2d0edad2dc964ab2d56e66e7ee8baf3ad
Blaise Fonseca <blaise.fonseca@un.org>
2025-06-12 21:22:59 -0400

    Adds new data types and refactors schema
    Introduces new data types for KM documents and indicator mappings.
    
    Refactors the EDocumentInfo schema to use a generic `Schemas` type for document bodies, improving type safety and flexibility.
    
    Corrects the `identifier` property type in `ETerm` to `string` for consistency.

--- types/controls/indicator-mapping.ts
+++ types/controls/indicator-mapping.ts
@@ -1,5 +1,14 @@
 export interface IndicatorMapping {
-    identifier: string
-    code: string
-    title: string
-  }
+  identifier: string
+  code: string
+  title: string
+}
+export interface IndicatorData {
+  indicatorCode : string
+  hasDisaggregation : string
+  disaggregation : string
+  year : Number
+  unit : string
+  value : Number
+  footnote : string
+}
