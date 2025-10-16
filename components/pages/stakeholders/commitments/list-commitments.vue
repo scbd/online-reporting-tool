fa3997a418d2ce305e7384d32b83a855a79c70a8
Blaise Fonseca <blaise.fonseca@un.org>
2025-10-15 20:28:16 -0400

    feat: Add public list for stakeholder commitments

---
+++ components/pages/stakeholders/commitments/list-commitments.vue
@@ -0,0 +1,20 @@
+<template>
+    <record-type-search :record-types="[SCHEMAS.REFERENCE_STAKEHOLDER_COMMITMENT]" 
+    :show-record-type="false" :show-targets="false" :show-goals="false">        
+    </record-type-search>    
+</template>
+<i18n src="@/i18n/dist/components/pages/stakeholders/commitments/list-commitment.json"></i18n>
+
+<script setup lang="ts">
+
+    import { useRoute } from 'vue-router';
+    import {stringifyQuery} from 'ufo'
+    import { useThesaurusStore }    from '@/stores/thesaurus';
+
+    const {t} = useI18n();
+    const route = useRoute();
+    const { $appRoutes } = useNuxtApp();
+    const thesaurusStore = useThesaurusStore ();
+    const customFilters  = ref({});
+    
+</script>
---
+++ pages/stakeholders/commitments/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <CCard>
+      <CCardHeader>
+        <slot name="header"> <CIcon name="cil-grid" /> {{ t('commitments') }} </slot>
+      </CCardHeader>
+      <CCardBody>        
+        <km-suspense>
+            <list-commitments></list-commitments>
+        </km-suspense>
+      </CCardBody>
+    </CCard>
+  </template>
+  <!-- <i18n src="@/i18n/dist/pages/stakeholders/commitments/index.json"></i18n> -->
+  <script setup lang="ts">
+
+    const { t, locale } = useI18n();
+    const route         = useRoute();
+    definePageMeta({
+        auth:false,
+        schema:SCHEMAS.REFERENCE_STAKEHOLDER_COMMITMENT,
+        breadcrumbs : {}
+    });
+
+  </script>
