5bd3037ee2ee132cefbac4662d2502849831de7b
Blaise Fonseca <blaise.fonseca@un.org>
2025-10-15 20:28:16 -0400

    feat: Add user-specific 'My Commitments' section with CRUD pages

---
+++ pages/stakeholders/commitments/my-commitments/[identifier]/edit.vue
@@ -0,0 +1,21 @@
+<template>
+  <km-suspense>
+    <edit-commitment></edit-commitment>
+  </km-suspense>
+</template>
+  
+<script setup lang="ts">
+
+    const schema      = useGetRealmSchema(SCHEMAS.REFERENCE_STAKEHOLDER_COMMITMENT)
+    const { locale }  = useI18n();
+    const route       = useRoute();
+    definePageMeta({
+        auth:true,
+        schema:SCHEMAS.REFERENCE_STAKEHOLDER_COMMITMENT,
+        roles:[ROLES.STAKEHOLDER_COMMITMENT_AUTHORIZED_USER],
+        breadcrumbs : {
+          skip : ['identifier']
+        }
+    })
+</script>
+  
