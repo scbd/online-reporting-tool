1a16e9063a612f5f3ec1a8a409ee65fe6e6918a3
Blaise Fonseca <blaise.fonseca@un.org>
2025-10-15 20:18:07 -0400

    Feat: Add new `KmValueList` component

---
+++ components/controls/view/km-value-list.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="km-value-list">
+        <km-value v-for="(item, index) in value" :key="index" :value="item" class="mb-1"/>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+    const props = defineProps({
+        value  : {type:Array<string>, required:true }
+    })
+
+    const { value } = toRefs(props);    
+
+
+</script>
