930941971a96d2589a719bc11f8ee1da2e977572
Blaise Fonseca <blaise.fonseca@un.org>
2025-12-12 16:00:08 -0500

    Block question options only. keep clear button open to user
    Refactors the question display component to improve the
    structure and loading experience. Moves loading overlay
    to the correct element and modifies the component structure
    for better organization.

--- components/controls/km-question.vue
+++ components/controls/km-question.vue
@@ -1,41 +1,38 @@
 <template>
     <template v-if="question">
-        <div class="vld-parent">
+
+        <km-form-group :name="question?.key" :required="question.mandatory && question.enabled" class="mb-0">
+            <template #caption>
+                <slot>
+                    {{ question?.number }} {{ question?.title }}
+                </slot>
+                <button class="ms-3 btn btn-danger btn-sm float-end" v-if="model"
+                    @click="model = undefined">{{ t('clear') }}</button>
+            </template>
+        </km-form-group>
+
+        <div class="question-options ms-3 vld-parent">
             <overlay-loading :active="!enabled" background-color="rgb(9 9 9)" :opacity="0.1"
                 :is-full-page="false">
                 &nbsp;
             </overlay-loading>
-
-            <km-form-group :name="question?.key" :required="question.mandatory && question.enabled" class="mb-0">
-                <template #caption>
-                    <slot>
-                        {{ question?.number }} {{ question?.title }}
-                    </slot>
-                    <button class="ms-3 btn btn-danger btn-sm float-end" v-if="model"
-                        @click="model = undefined">{{ t('clear') }}</button>
-                </template>
-            </km-form-group>
-
-            <div class="question-options ms-3">
-                <div v-if="question.type == 'option'">
-                    <km-form-group v-if="question?.options" class="mb-0">
-                        <km-form-check-item v-for="option in question?.options" :key="option.value" type="radio"
-                            :name="'option_' + question.key" :for="'option_' + question.key + '_' + option.value"
-                            :id="'option_' + question.key + '_' + option.value" :value="option.value" v-model="model"
-                            :label="lstring(option.title, locale)" />
-                    </km-form-group>
-                </div>
-                <div v-else-if="question.type == 'checkbox' && question?.options">
-                    <km-multi-checkbox v-model="multiOptionModel" option-value-field="value"
-                        :options="question?.options">
-                    </km-multi-checkbox>
-                </div>
-                <div v-else-if="question.type == 'string'">
-                    <input :id="question?.key" :placeholder="question?.title" class="form-control" v-model="model"
-                        type="textbox" />
-                </div>
+            <div v-if="question.type == 'option'">
+                <km-form-group v-if="question?.options" class="mb-0">
+                    <km-form-check-item v-for="option in question?.options" :key="option.value" type="radio"
+                        :name="'option_' + question.key" :for="'option_' + question.key + '_' + option.value"
+                        :id="'option_' + question.key + '_' + option.value" :value="option.value" v-model="model"
+                        :label="lstring(option.title, locale)" />
+                </km-form-group>
             </div>
-
+            <div v-else-if="question.type == 'checkbox' && question?.options">
+                <km-multi-checkbox v-model="multiOptionModel" option-value-field="value"
+                    :options="question?.options">
+                </km-multi-checkbox>
+            </div>
+            <div v-else-if="question.type == 'string'">
+                <input :id="question?.key" :placeholder="question?.title" class="form-control" v-model="model"
+                    type="textbox" />
+            </div>
         </div>
     </template>
 </template>
