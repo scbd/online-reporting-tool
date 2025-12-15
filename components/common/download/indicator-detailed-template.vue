896012a5a55d00412eac21cb8f634fef00b4d5eb
Blaise Fonseca <blaise.fonseca@un.org>
2025-12-15 17:53:47 -0500

    Adds detailed indicator templates download
    Provides users with access to more detailed indicator templates for download.
    
    These templates include optional disagregations, aligning with decision 16/32, and can be attached to national reports.
    
    Includes i18n support for user interface elements.

---
+++ components/common/download/indicator-detailed-template.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="alert alert-info" v-if="detailIndicatorTemplateMapping[indicator.identifier]">
+        <strong>{{t('optional')}}</strong>
+        <div>
+            {{ t('newIndicatorTemplates') }}
+            <a :href="detailIndicatorTemplateMapping[indicator.identifier]" target="_blank">
+                {{ t('download') }}  
+                <font-awesome-icon icon="fa-download"></font-awesome-icon>                                                            
+            </a>
+        </div>
+    </div>
+</template>
+
+<i18n src="@/i18n/dist/components/common/download/indicator-detailed-template.json"></i18n>
+
+<script setup lang="ts">
+
+    const props = defineProps({
+        indicator: { type: Object, required: true }
+    });
+
+    const detailIndicatorTemplateMapping:Record<string, string> = {
+        "GBF-INDICATOR-2.1"  : "/excel-templates/GBF-INDICATOR-2.1.xlsx",
+        "GBF-INDICATOR-2.2"  : "/excel-templates/GBF-INDICATOR-2.1.xlsx", //obsolete and use 2.1
+        "GBF-INDICATOR-12.1" : "/excel-templates/GBF-INDICATOR-12.1.xlsx",
+        "GBF-INDICATOR-A.1"  : "/excel-templates/GBF-INDICATOR-A.1.xlsx",
+        "GBF-INDICATOR-A.2"  : "/excel-templates/GBF-INDICATOR-A.2.xlsx",
+        "GBF-INDICATOR-B1"   : "/excel-templates/GBF-INDICATOR-B1.xlsx"
+    }
+
+    const { t, locale } = useI18n();
+    
+</script>
+
+<style scoped>
+
+</style>
