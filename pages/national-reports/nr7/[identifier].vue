111a992600a12c984acf817918fa79dff2b999ee
Blaise Fonseca <blaise.fonseca@un.org>
2026-02-23 14:30:43 -0500

    Enhances national indicator handling in NR7 (#227)

--- pages/national-reports/nr7/[identifier].vue
+++ pages/national-reports/nr7/[identifier].vue
@@ -1,11 +1,13 @@
 <template>
     <km-suspense>
-      <view-actions print-selector=".nr7-section-view" :title="lstring(record?.workingDocumentTitle||record?.title, locale)"></view-actions>
+      <view-actions print-selector=".nr7-section-view" :title="lstring(record?.workingDocumentTitle||record?.title, locale)"
+          ref="viewActionsRef" @on-print-document="onPrintDocument" @on-before-print="onBeforePrint"
+          @on-after-print="onAfterPrint" @on-pdf-document="onPdfDocument" @on-after-pdf="onAfterPdf"></view-actions>
 
       <workflow-request v-if="workflowId" :workflow-id="workflowId"></workflow-request>
 
       <div class="print-section">
-        <nr-7-view class="print-section" :identifier="identifier" :public-data="true" @on-document-load="onDocumentLoad"></nr-7-view>
+        <nr-7-view class="print-section" :is-printing="isPrinting" :identifier="identifier" :public-data="true" @on-document-load="onDocumentLoad"></nr-7-view>
       </div>
     </km-suspense>
 
@@ -20,6 +22,7 @@
   const identifier = ref(route.params.identifier);
   const record     = ref(null);
   const workflowId = ref(route.query?.workflowId);
+  const isPrinting = ref(false);
   
   definePageMeta({
     breadcrumbs : {
@@ -30,6 +33,23 @@
   const onDocumentLoad = (document)=>{
     record.value = document;
   }
+
+  const onPrintDocument = () => {
+      isPrinting.value = true;
+  }
+  const onBeforePrint = () => {
+      isPrinting.value = true;
+  }
+  const onAfterPrint = () => {
+      isPrinting.value = false;
+  }
+
+  const onPdfDocument = () => {
+      isPrinting.value = true;
+  }    
+  const onAfterPdf = () => {
+      isPrinting.value = false;
+  }  
 </script>
 
 <style scoped>
