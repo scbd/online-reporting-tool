25159aea10477c566ae850dbd9892895a2288851
Blaise Fonseca <blaise.fonseca@un.org>
2026-05-25 08:50:08 -0500

    Enhances Search and Pagination Components (#247)

--- components/pages/stakeholders/commitments/list-commitments.vue
+++ components/pages/stakeholders/commitments/list-commitments.vue
@@ -1,6 +1,20 @@
 <template>
     <record-type-search :record-types="[SCHEMAS.REFERENCE_STAKEHOLDER_COMMITMENT]" 
-    :show-record-type="false" :show-targets="false" :show-goals="false" country-region-field="countryReviews">        
+    :show-record-type="false" :show-targets="false" :show-goals="false" 
+    country-search-field="countryReviews_ss" region-search-field="countryReviews_REL_ss">        
+        <template #metadata="{document}">
+            <slot name="metadata" :document="document">
+                <small class="me-2 fs-6">{{formatDate(document.recCreationDate)}}</small>|
+                <small class="me-2 fs-6">{{document.schema_EN_s}}</small>|
+                <small class="me-2 fs-6" v-if="document.organization_t">{{document.organization_t}}</small>
+                <small class="me-2 fs-6"  v-if="document.countryReviews_EN_txt">|
+                    {{document.countryReviews_EN_txt.join(', ')}}
+                </small>
+                <small class="me-2 fs-6" v-if="document.primaryGlobalAlignment_s">|
+                    {{document.primaryGlobalAlignment_s}}
+                </small>
+            </slot>
+        </template>
     </record-type-search>    
 </template>
 <i18n src="@/i18n/dist/components/pages/stakeholders/commitments/list-commitment.json"></i18n>
