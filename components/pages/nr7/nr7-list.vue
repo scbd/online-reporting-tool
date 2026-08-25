4269dffb4608187d9d0a6c42c61e0b4a1f9397bf
Blaise Fonseca <blaise.fonseca@un.org>
2026-08-25 15:04:42 -0500

    Extracts reusable document dates component (#251)
    Co-authored-by: RJH <randy.houlahan@cbd.int>

--- components/pages/nr7/nr7-list.vue
+++ components/pages/nr7/nr7-list.vue
@@ -6,25 +6,17 @@
                 <slot name="metadata" :document="document">
                     <small class="me-2 fs-6">{{document.schema_EN_s}}</small>|
                     <small class="me-2 fs-6" v-if="document.government_EN_s">{{document.government_EN_s}} |</small>
-                    <small class="me-2 fs-6">
-                        <strong>{{t('publishedOn')}}</strong> {{formatDate(document.recCreationDate)}}
-                    </small>
-                    <small class="me-2 fs-6"  v-if="document.recDate!=document.recCreationDate">
-                        {{t('updated')}} {{formatDate(document.recDate)}}
-                    </small>
+                    <document-dates :published-on="document.recCreationDate" :updated-on="document.recDate"></document-dates>
                 </slot>
             </template>
         </record-type-search>
     </div>
 </template>
-<i18n src="@/i18n/dist/components/pages/nr7/nr7-list.json"></i18n>
 <script setup lang="ts">
 //@ts-nocheck
 import searchResult from '@/components/controls/search/search-result.vue';
 import { SCHEMAS } from '@/utils';
 
-    const { t } = useI18n();
-    
     const recordTypes = [SCHEMAS.NATIONAL_REPORT_7];//, SCHEMAS.NATIONAL_REPORT_7_BINARY_INDICATOR_DATA, SCHEMAS.NATIONAL_REPORT_7_INDICATOR_DATA];
 
 
